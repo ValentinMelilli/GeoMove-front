86d244b0eaaee8636a4a7a202153e5cb09248830
ValentinMelilli <melilli.valentin@gmail.com>
2023-06-13 15:48:07 +0200

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Géo Move",
+      htmlAttrs: {
+        lang: "fr"
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+})

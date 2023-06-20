4fead86c08b571235548498f596b4ece28e395bf
ValentinMelilli <melilli.valentin@gmail.com>
2023-06-20 15:09:58 +0200

    Fixed markers bug

--- components/block/map.vue
+++ components/block/map.vue
@@ -3,7 +3,7 @@
         <Transition name="appear">
             <BlockStructureModal v-show="showModal" :blockData="selectedStructure" class="right-20 top-1/2" />
         </Transition>
-        <l-map :useGlobalLeaflet="false" :zoom="13" :center="[43.613599, 3.873309]" ref="map" :options="{ zoomControl: false }">
+        <l-map :useGlobalLeaflet="false" :zoom="13" :center="[43.613599, 3.873309]" ref="map" :options="{ zoomControl: false }" @ready="createMarkers">
             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
             <l-control-zoom position="bottomleft"></l-control-zoom>
         </l-map>
@@ -21,19 +21,19 @@ let selectedStructure = ref({});
 
 const { data } = await useFetch('/api/structures', { method: 'GET' });
 
-onMounted(async () => {
+async function createMarkers() {
     const { marker } = await import('leaflet');
 
     data?.value?.forEach((structure : any) => {
         const m: any = marker(structure.coord)
-        .addTo(map.value.leafletObject);
+        .addTo(map?.value?.leafletObject);
         /*.bindPopup(`<b>${structure.name}</b><br>${structure.address}<br>${structure.phone}<br>${structure.email}<br>${structure.website}`);*/
         m.on('click', function(e: any) {
             showModal.value = true;
             selectedStructure.value = structure;
         });
     });
-})
+}
 </script>
 
 <style scoped lang="postcss">

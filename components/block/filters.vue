78a2bd9ac58c944d5c8000a2aa7c32d1c970c022
ValentinMelilli <melilli.valentin@gmail.com>
2023-06-20 15:22:18 +0200

    Fixed filter buttons

--- components/block/filters.vue
+++ components/block/filters.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="w-full p-8 bg-accent-2">
         <div class="flex flex-row">
-            <div class="rounded-full bg-white m-auto w-fit flex flex-row py-2 px-6">
-                <div class="flex flex-row">
+            <div class="rounded-full bg-white m-auto w-1/2 flex flex-row py-2 px-6">
+                <button class="flex flex-row text-accent-2 duration-100 hover:text-primary w-1/3">
                     <IconCompass class="h-6 w-6 mr-4" />
-                    <input type="text" placeholder="Lieu" class="focus:outline-none" />
-                </div>
+                    <span>Lieu</span>
+                </button>
                 <div class="bg-gray-300 w-px mx-4"></div>
-                <div class="flex flex-row">
+                <button class="flex flex-row text-accent-2 duration-100 hover:text-primary w-1/3">
                     <IconCalendar class="h-6 w-6 mr-4" />
-                    <input type="text" placeholder="Périodicité" class="focus:outline-none" />
-                </div>
+                    <span>Périodicité</span>
+                </button>
                 <div class="bg-gray-300 w-px mx-4"></div>
-                <div class="flex flex-row">
+                <button class="flex flex-row text-accent-2 duration-100 hover:text-primary w-1/3">
                     <IconBook class="h-6 w-6 mr-4" />
-                    <input type="text" placeholder="Cours" class="focus:outline-none" />
-                </div>
+                    <span>Cours</span>
+                </button>
             </div>
             <button class="rounded-full bg-white absolute right-20 w-fit flex flex-row py-2 px-6 duration-200 hover:text-primary hover:stroke-primary">
                 <IconControls class="h-6 w-6 mr-4" />
